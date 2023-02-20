4df7924cc930b97f4c92b9431ded643d305bc5db
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-20 19:21:26 +0600

    problem 3 solved

--- practice-problem03.js
+++ practice-problem03.js
@@ -5,3 +5,11 @@ b) Calculate the sum of the squared elements
 c) Return the average of the sum of the squared elements
 Print the result. 
 */
+
+const newFunction = (numbers) => {
+    return (numbers.map(num=> num*num)).reduce((a , b) => 
+    {return a + b}, 0) / numbers.length;
+}
+const numbers = [5, 7, 3, 4, 2, 6];
+const display = newFunction(numbers);
+console.log(display);
