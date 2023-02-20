783c1e7d869e6788f0a90c613f1654cd90adeef4
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-20 21:59:11 +0600

    second problem solving

--- practice-problem02.js
+++ practice-problem02.js
@@ -7,3 +7,17 @@ b) Check if the length of each element is even, push elements
 with even length to a new array and return the result
 Print the result. 
 */
+
+const nameOfFriends = (friendsArray) => {
+    let evenNames = [];
+    for (let i = 0; i < friendsArray.length; i++){
+        const element = friendsArray[i];
+        if(element.length % 2 == 0){
+            evenNames.push(element);
+        }
+    }
+    return evenNames;
+}
+
+const friendsNames = ["Sabiha" , "Rafi" , "Mosharof" , "Munna" , "Jahid"];
+const even = nameOfFriends(friendsNames);
